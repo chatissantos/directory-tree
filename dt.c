100170148668e3857f1038b7ed866cd1285d681d
chatis <chatis@becurity.com>
2019-09-15 15:18:39 -0500

    added printDir implementation

--- dt.c
+++ dt.c
@@ -3,16 +3,29 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <string.h>
+#include <dirent.h>
 
 void printError(char* message);
-
+void printDir(
+        char* directory
+        , int symbolicLinkFlag
+        , int timeFlag
+        , int gidFlag
+        , int numberOfLinksFlag
+        , int permissionFlag
+        , int sizeFlag
+        , int fileTypeFlag
+        , int uidFlag
+        , int lflag
+        , int indentSize
+);
 int main(int argc, char *argv[]) {
     int symbolicLinkFlag = 0, timeFlag = 0, gidFlag = 0, numberOfLinksFlag = 0, permissionFlag = 0, sizeFlag = 0, fileTypeFlag = 0, uidFlag = 0, lflag = 0, indentSize = 4;
     int opt;
-    while((opt = getopt(argc, argv, "hI:Ldgipstul")) != -1)
-    {
-        switch(opt)
-        {
+    int index;
+    char* directory;
+    while((opt = getopt(argc, argv, "hI:Ldgipstul")) != -1) {
+        switch(opt) {
             case 'h':
                 printf("print help and exit\n");
                 return 0;
@@ -25,7 +38,7 @@ int main(int argc, char *argv[]) {
                 break;
             case 'L':
                 symbolicLinkFlag = 1;
-                printf("L flag\n");
+                printf("Follow symbolic links flag\n");
                 break;
             case 'd':
                 timeFlag = 1;
@@ -67,11 +80,52 @@ int main(int argc, char *argv[]) {
                 return -1;
         }
     }
+    if (optind == argc) {
+        strcpy(directory, ".");
+    } else {
+        strcpy(directory, argv[optind]);
+    }
+    printDir(directory, symbolicLinkFlag, timeFlag, gidFlag, numberOfLinksFlag, permissionFlag, sizeFlag, fileTypeFlag, uidFlag, lflag, indentSize);
     return 0;
 }
 
-void printDir(symbolicLinkFlag, timeFlag, gidFlag, numberOfLinksFlag, permissionFlag, sizeFlag, fileTypeFlag, uidFlag, lflag, indentSize) {
-
+void printDir(
+        char* directory
+        , int symbolicLinkFlag
+        , int timeFlag
+        , int gidFlag
+        , int numberOfLinksFlag
+        , int permissionFlag
+        , int sizeFlag
+        , int fileTypeFlag
+        , int uidFlag
+        , int lflag
+        , int indentSize
+        ) {
+    int index;
+    struct dirent *de;
+    DIR *dr = opendir(directory);
+    if (dr == NULL) {
+        char str[1000];
+        strcpy(str, directory);
+        printError(strcat(str, " is not a directory"));
+        return;
+    }
+    while ((de = readdir(dr)) != NULL) {
+        for (index = 0; index < indentSize; index++) {
+            printf(" ");
+        }
+        printf("%s\t\t\t\t\t%hhu\n", de->d_name, de->d_type);
+        if (de->d_type == 4 && (strncmp(de->d_name, ".", 1) != 0) && (strncmp(de->d_name, "..", 2) != 0)) {
+            char newDir[1000];
+            strcpy(newDir, directory);
+            strcat(newDir, "/");
+            strcat(newDir, de->d_name);
+            printDir(newDir, symbolicLinkFlag, timeFlag, gidFlag, numberOfLinksFlag, permissionFlag, sizeFlag, fileTypeFlag, uidFlag, lflag, indentSize + 4);
+        }
+    }
+    closedir(dr);
+    return;
 }
 
 void printError(char* message) {
